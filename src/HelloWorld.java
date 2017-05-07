5dfd4064f359dd15fa9d6da5c53e4c41a3f537f9
Dave Gonzalez <dgonzalez@thosegonzos.com>
2017-05-07 14:02:10 -0500

    ..with code...

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Java 8");
+		
+		System.out.println("Java version: " + System.getProperty("java.version"));
+
+	}
+
+}
